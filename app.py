993bfa2d1ec945120bbf9b357a72484346497e44
badri-13650 <badrinarayanan.r@zohocorp.com>
2023-02-02 13:26:50 +0530

    finished display_menu and made some refactoring changes in app.py and added more meta info to meta_book_information.json file

--- app.py
+++ app.py
@@ -5,12 +5,57 @@ import json
 class BookManager:
     def __init__(self, db_file_name):
         self.database = DataBase(db_file_name)
+        self.database.create_storage()
         self.max_print_length = 50
+
+        self.user_options = {
+            "1": {
+                "display_text": "Add a new book",
+                "execute": self.store_book
+            },
+            "2": {
+                "display_text": "Display all available books",
+                "execute": self.display_all_books
+            },
+            "3": {
+                "display_text": "Search specific book by book name",
+                "execute": self.find_book
+            },
+            "4": {
+                "display_text": "Read a Book",
+                "execute": self.mark_book_as_read
+            },
+            "5": {
+                "display_text": "Delete a Book",
+                "execute": self.delete_book
+            }
+        }
+
         with open("meta_book_information.json") as meta_book_info_file:
             self.book_info_needed = json.load(meta_book_info_file)
 
+    @staticmethod
+    def get_book_name(message="Enter the Book Name : "):
+        book_name = input(message)
+
+        while len(book_name) == 0:
+            print("Please Enter valid Book Name!!!")
+            book_name = input(message)
+
+        return book_name
+
+    def print_line_seperator(self, seperator="-"):
+        print(seperator * self.max_print_length)
+
     def display_user_menu(self):
-        raise NotImplementedError("Yet to Implement this section!!!")
+        self.print_line_seperator()
+        print("USER MENU".center(self.max_print_length))
+        self.print_line_seperator()
+
+        for option, content in self.user_options.items():
+            print(f"{option}. {content['display_text']}")
+
+        self.print_line_seperator()
 
     def store_book(self):
         book_info = dict()
@@ -34,13 +79,13 @@ class BookManager:
         list_of_books = self.database.get_all_books()
 
         for book_no, book in enumerate(list_of_books):
-            print("-" * self.max_print_length)
+            self.print_line_seperator()
             print(f"Book {book_no}")
-            print("-" * self.max_print_length)
+            self.print_line_seperator()
 
             self.display_book_info(book)
 
-            print("-" * self.max_print_length)
+            self.print_line_seperator()
 
         print("All books are printed successfully!!")
 
@@ -50,7 +95,7 @@ class BookManager:
         return book
 
     def find_book(self):
-        book_name = input("Enter the Book Name to find : ")
+        book_name = self.get_book_name("Enter the Book Name to find : ")
 
         book = self.search_book(book_name)
 
@@ -62,7 +107,7 @@ class BookManager:
         self.display_book_info(book)
 
     def delete_book(self):
-        book_name = input("Enter the Book Name to delete : ")
+        book_name = self.get_book_name("Enter the Book Name to Delete : ")
 
         book = self.search_book(book_name)
 
@@ -73,23 +118,22 @@ class BookManager:
         self.database.delete_book(book_name)
 
     def mark_book_as_read(self):
-        raise NotImplementedError("Yet to Implement this section!!!")
+        book_name = self.get_book_name("Enter the Book Name to Read : ")
+        book = self.search_book(book_name)
+
+        if not book:
+            print("Sorry the book you are trying to read is not present!!!")
+            return
+
+        self.database.mark_book_as_read(book_name)
 
     def run(self):
         self.display_user_menu()
         user_input = input("Enter one of the option above : ")
 
-        user_options = {
-            "1": self.store_book,
-            "2": self.display_all_books,
-            "3": self.find_book,
-            "4": self.mark_book_as_read,
-            "5": self.delete_book
-        }
-
         while user_input != "q":
-            if user_input in user_options:
-                user_options[user_input]()
+            if user_input in self.user_options:
+                self.user_options[user_input]["execute"]()
             else:
                 print("Please Enter valid user option available!!")
 
@@ -97,3 +141,8 @@ class BookManager:
             user_input = input("Enter one of the option above : ")
 
         print('Thank you for your interaction with "Book Manager" your responses are being saved safely!!!')
+
+
+if __name__ == "__main__":
+    database_file_name = "data.db"
+    BookManager(database_file_name).run()
