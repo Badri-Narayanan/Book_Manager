21b27403199e8b817fff309d39affc43dba1dc7e
BADRI NARAYANAN <79736071+Badri-Narayanan@users.noreply.github.com>
2023-02-02 15:38:15 +0530

    User Module (#2)
    * partially implemented BookManager
    
    * added database schema
    
    * added __pycache__ to .gitignore file
    
    * finished display_menu and made some refactoring changes in app.py and added more meta info to meta_book_information.json file
    
    ---------
    
    Co-authored-by: badri-13650 <badrinarayanan.r@zohocorp.com>

---
+++ app.py
@@ -0,0 +1,148 @@
+from Model.database import DataBase, Dict, Union
+import json
+
+
+class BookManager:
+    def __init__(self, db_file_name):
+        self.database = DataBase(db_file_name)
+        self.database.create_storage()
+        self.max_print_length = 50
+
+        self.user_options = {
+            "1": {
+                "display_text": "Add a new book",
+                "execute": self.store_book
+            },
+            "2": {
+                "display_text": "Display all available books",
+                "execute": self.display_all_books
+            },
+            "3": {
+                "display_text": "Search specific book by book name",
+                "execute": self.find_book
+            },
+            "4": {
+                "display_text": "Read a Book",
+                "execute": self.mark_book_as_read
+            },
+            "5": {
+                "display_text": "Delete a Book",
+                "execute": self.delete_book
+            }
+        }
+
+        with open("meta_book_information.json") as meta_book_info_file:
+            self.book_info_needed = json.load(meta_book_info_file)
+
+    @staticmethod
+    def get_book_name(message="Enter the Book Name : "):
+        book_name = input(message)
+
+        while len(book_name) == 0:
+            print("Please Enter valid Book Name!!!")
+            book_name = input(message)
+
+        return book_name
+
+    def print_line_seperator(self, seperator="-"):
+        print(seperator * self.max_print_length)
+
+    def display_user_menu(self):
+        self.print_line_seperator()
+        print("USER MENU".center(self.max_print_length))
+        self.print_line_seperator()
+
+        for option, content in self.user_options.items():
+            print(f"{option}. {content['display_text']}")
+
+        self.print_line_seperator()
+
+    def store_book(self):
+        book_info = dict()
+
+        for info in self.book_info_needed:
+            meta_book_info = self.book_info_needed[info]
+
+            book_info[info] = eval(meta_book_info["value"])
+
+        self.database.store_book(book_info)
+
+        print("Your Book is saved successfully!!!")
+
+    def display_book_info(self, book: dict) -> None:
+        for info in self.book_info_needed:
+            meta_book_info = self.book_info_needed[info]
+
+            print(f"{meta_book_info['display_text']} : {book[info]}")
+
+    def display_all_books(self):
+        list_of_books = self.database.get_all_books()
+
+        for book_no, book in enumerate(list_of_books):
+            self.print_line_seperator()
+            print(f"Book {book_no}")
+            self.print_line_seperator()
+
+            self.display_book_info(book)
+
+            self.print_line_seperator()
+
+        print("All books are printed successfully!!")
+
+    def search_book(self, book_name: str) -> Dict[str, Union(str, bool)]:
+        book = self.database.get_book_details(book_name)
+
+        return book
+
+    def find_book(self):
+        book_name = self.get_book_name("Enter the Book Name to find : ")
+
+        book = self.search_book(book_name)
+
+        if not book:
+            print("Sorry the Book you are finding is not present in our Data Base!!!")
+            return
+
+        print("Book Info:")
+        self.display_book_info(book)
+
+    def delete_book(self):
+        book_name = self.get_book_name("Enter the Book Name to Delete : ")
+
+        book = self.search_book(book_name)
+
+        if not book:
+            print("Sorry the book you are trying to delete is not present in our Data Base!!!")
+            return
+
+        self.database.delete_book(book_name)
+
+    def mark_book_as_read(self):
+        book_name = self.get_book_name("Enter the Book Name to Read : ")
+        book = self.search_book(book_name)
+
+        if not book:
+            print("Sorry the book you are trying to read is not present!!!")
+            return
+
+        self.database.mark_book_as_read(book_name)
+
+    def run(self):
+        self.display_user_menu()
+        user_input = input("Enter one of the option above : ")
+
+        while user_input != "q":
+            if user_input in self.user_options:
+                self.user_options[user_input]["execute"]()
+            else:
+                print("Please Enter valid user option available!!")
+
+            self.display_user_menu()
+            user_input = input("Enter one of the option above : ")
+
+        print('Thank you for your interaction with "Book Manager" your responses are being saved safely!!!')
+
+
+if __name__ == "__main__":
+    database_file_name = "data.db"
+    BookManager(database_file_name).run()
